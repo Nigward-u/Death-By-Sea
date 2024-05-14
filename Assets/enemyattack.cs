5a538d7f8dd37a85317b5f52868ba7d4a64b7882
vezamm <149106775+vezamm@users.noreply.github.com>
2024-05-14 18:53:49 +0100

    attack

--- Assets/enemyattack.cs
+++ Assets/enemyattack.cs
@@ -6,19 +6,17 @@ public class enemyattack : MonoBehaviour
 {
 
     public enemybehavior enemy;
-<<<<<<< Updated upstream
-=======
     public GameObject Player;
     
->>>>>>> Stashed changes
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-<<<<<<< Updated upstream
+
             enemy.attack();
-=======
+
             if (enemy.ReadyToAttack)
             {
 
@@ -28,7 +26,7 @@ public class enemyattack : MonoBehaviour
             }
             ;
             //enemy.attack();
->>>>>>> Stashed changes
+
         }
     }
 }
