50812f99bd7ec4848e246b545c2eb98afb650c58
Nigward-u <75142755+Nigward-u@users.noreply.github.com>
2024-05-15 01:45:52 +0100

    qzdzqd

--- Assets/scripts/BossAttack.cs
+++ Assets/scripts/BossAttack.cs
@@ -1,34 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.Examples;
 using UnityEngine;
 
 public class BossAttack : MonoBehaviour
 {
     public Transform[] spawnlocation;
     public GameObject bullet;
-    public bool isAttacking=true;
+    public bool isAttacking = true;
     private int spawnpoint;
-    // Start is called before the first frame update
+    public int nb_Bullets = 20;
+    public bool isResting = false;
+    public float attackDelay = 0.3f;
+    public float reloadTime = 5f;
+    public GameObject bubble;
+    public Transform bubblepos;
+    public bool spawnbubble=false;
+    public int hp = 4;
+
     void Start()
     {
-        
+        StartCoroutine(AttackRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator AttackRoutine()
     {
-        if (isAttacking)
+        while (true)
         {
-            spawnpoint = Random.Range(0, spawnlocation.Length);
+            if (isAttacking && nb_Bullets > 0)
+            {
+                spawnpoint = Random.Range(0, spawnlocation.Length);
+                Instantiate(bullet, spawnlocation[spawnpoint].position, Quaternion.identity);
+                nb_Bullets--;
+                yield return new WaitForSeconds(attackDelay);
+                spawnbubble = true;
+            }
+            if (spawnbubble && nb_Bullets == 0)
+            {
+                Instantiate(bubble, bubblepos.position, Quaternion.identity);
+                spawnbubble = false;
+                
+            }
+            else if (nb_Bullets == 0 && !isResting)
+            {
+                isResting = true;
+                isAttacking = false;
+                yield return new WaitForSeconds(reloadTime);
+                isAttacking = true;
+                isResting = false;
+                nb_Bullets = 20;
+                
+            }
             
             
-            Instantiate(bullet, spawnlocation[spawnpoint].position,Quaternion.identity);
-            isAttacking = false;
-            Invoke("resetattack", 0.3f);
+            yield return null;
+            if (hp == 0)
+            {
+                Destroy(gameObject);
+            }
         }
-    }
-    void resetattack()
-    {
-        isAttacking =true;
     }
 }
